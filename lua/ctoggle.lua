15d4253eea8a4f99d5b59193c89b356b1f50f4d2
pantz8622 <pantz8622@gmail.com>
2023-02-23 12:42:08 +0800

    v0.1

---
+++ lua/ctoggle.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+local function qf_isopen()
+  local r = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win.quickfix == 1 then
+      r = true
+    end
+  end
+  return r
+end
+
+M.ctoggle = function ()
+  if qf_isopen() then
+    vim.cmd "cclose"
+  else
+    vim.cmd "copen"
+  end
+end
+
+M.cc_wo_focus = function()
+  local qfwin = vim.api.nvim_get_current_win();
+  vim.cmd ".cc"
+  vim.api.nvim_set_current_win(qfwin)
+end
+
+return M
